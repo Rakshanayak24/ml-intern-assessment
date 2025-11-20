8fe4f704c79def2955a2c5cd56fbf4a59f051db2
Raksha Nayak <118899722+Rakshanayak24@users.noreply.github.com>
2025-11-21 00:50:12 +0530

    Update utils.py

--- ml-assignment/src/utils.py
+++ ml-assignment/src/utils.py
@@ -1,2 +1,14 @@
 # This file is optional.
 # You can add any utility functions you need for your implementation here.
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"[^a-z0-9\s]", "", text)
+    return text
+
+def tokenize(text):
+    if not text.strip():
+        return []
+    return text.split()
+
